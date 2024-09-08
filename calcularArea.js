d2bcfbd35f57f844a89770e40698d741084acd8c
jaipe5 <117586552+jaipe5@users.noreply.github.com>
2024-09-08 22:54:41 +0200

    First commit

---
+++ calcularArea.js
@@ -0,0 +1,7 @@
+//Esta función calcula el área de un círculo dado su radio
+function calcularArea(radio){
+    return Math.PI * Math.pow(radio,2);
+}
+
+//Ejemplo de uso de la función
+console.log(calcularArea(5)); //78.54
